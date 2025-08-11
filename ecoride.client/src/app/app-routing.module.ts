7cc6b28895cb9f0cba1e19ce568ccf1bb341fd94
Allan MARCELLE <allan.marcelle@myconsultingit.onmicrosoft.com>
2025-08-11 16:07:04 +0200

    Page préliminaire avant vue des covoiturage  pour remplir formulaire précisant : la date de départ, la ville de départ et d'arrivée. Après envoie affiche la page de vue.

--- ecoride.client/src/app/app-routing.module.ts
+++ ecoride.client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ItineraireformComponent } from './itineraireform/itineraireform.component';
+import { ItinerairevueComponent } from './itinerairevue/itinerairevue.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'accueil', component: AccueilComponent },
   { path: 'itineraireform', component: ItineraireformComponent },
+  { path: 'itinerairevue', component: ItinerairevueComponent },
   { path: '', redirectTo: '/accueil', pathMatch: 'full' } // Redirige vers la page1 par défaut];
 ];
 
